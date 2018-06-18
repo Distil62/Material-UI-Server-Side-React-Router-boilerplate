b4a6316b98cb2d4802054c01189f01d263e73837
Charavner Louis <louis.charavner@nept-in.com>
2018-06-18 19:33:04 +0200

    master hihih

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,13 +3,6 @@ import { StaticRouter, Router } from "react-router";
 import Routes from './Routes/Routes.jsx';
 
 export default class Main extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			context : {}
-		}
-	}
-	
 	render() {
 		return (
 			<html lang="en">
