027d86cfcd349710bfbd40980bfc18c657ae0b4f
Charavner Louis <louis.charavner@nept-in.com>
2018-06-22 20:23:56 +0200

    master i will add hovercss

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -10,7 +10,7 @@ export class Blog extends Component {
     constructor(props) {
         super(props);
         const root = {
-            backgroundColor: Grey["500"]
+            backgroundColor: Grey["100"]
         }
         this.state = {
             root
@@ -23,7 +23,7 @@ export class Blog extends Component {
         <TopPage 
             img='https://cdn.discordapp.com/attachments/266953688801738752/459500282032685080/branden-harvey-362111-unsplash.jpg'
             title='Blog'/>
-        <div className='blog-padding' style={this.state.root}>
+        <div className='blog-content' style={this.state.root}>
             <Grid container justify='center'>
                 <Grid item xs={11}>
                     <HomeCard   img='https://cdna.artstation.com/p/assets/images/images/010/307/676/large/louis-charavner-flatspace.jpg?1523730134'
